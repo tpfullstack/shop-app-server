a1558c9c0207786a5b5ecf2b571cbc7cc003e5fa
KKarim <karim.kassa@univ-rouen.fr>
2025-01-15 08:35:37 +0100

    fixBugs(DocToEntity): Fix mapping fields from document type to JPA entity

--- src/main/java/fr/fullstack/shopapp/config/ElasticConfig.java
+++ src/main/java/fr/fullstack/shopapp/config/ElasticConfig.java
@@ -3,13 +3,35 @@ package fr.fullstack.shopapp.config;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomConversions;
+
+import java.util.Arrays;
 
 @Configuration
 public class ElasticConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        SimpleModule customModule = new SimpleModule();
+        objectMapper.registerModule(customModule);
+        return objectMapper;
+    }
+
+    @Bean
+    public ElasticsearchCustomConversions elasticsearchCustomConversions() {
+        return new ElasticsearchCustomConversions(
+                Arrays.asList(new LongToLocalTimeConverter())
+        );
+    }
 
     @Bean
     public ElasticsearchClient elasticsearchClient() {
